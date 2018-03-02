bf65ccb91162c22c36a0da054d5b2e8c844e15c8
Alejok28 <alejo_k28@hotmail.com>
2018-03-01 21:54:11 -0500

    Testing solution

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showInfo: false,
+      text: ''
+    }
+  }
+  handleChange(e) {
+    this.setState({
+      showInfo: e.target.checked
+    })
+    if (this.state.showInfo === false) {
+      this.setState({
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      })
+    }else {
+      this.setState({
+        text: ''
+      })
+    }
+  }
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label><input type="checkbox" checked={this.state.showInfo} onChange={this.handleChange.bind(this)}/> Mostrar información importante</label>
+        <p>{this.state.text}</p>
       </div>
     );
   }
