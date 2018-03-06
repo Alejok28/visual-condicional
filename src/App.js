036b0cf90b1d94f74977bef86f4e46602408c1b1
Alejok28 <alejo_k28@hotmail.com>
2018-03-06 17:33:43 -0500

    Final solution

--- src/App.js
+++ src/App.js
@@ -2,33 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor() {
+  constructor(){
     super();
-
     this.state = {
-      showInfo: false,
-      text: ''
-    }
+      showInfo: false
+    };
   }
-  handleChange(e) {
-    this.setState({
-      showInfo: e.target.checked
-    })
-    if (this.state.showInfo === false) {
+
+  handleClick(e) {
+    if (this.state.showInfo) {
       this.setState({
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      })
+        showInfo: false
+      });
     }else {
       this.setState({
-        text: ''
-      })
+        showInfo: true
+      });
     }
   }
   render() {
+    const p = <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>;
     return (
       <div className="wrapper">
-        <label><input type="checkbox" checked={this.state.showInfo} onChange={this.handleChange.bind(this)}/> Mostrar información importante</label>
-        <p>{this.state.text}</p>
+        <label><input type="checkbox" checked={this.state.showInfo} onClick={this.handleClick.bind(this)}/> Mostrar información importante</label>
+        { this.state.showInfo ? p : null}
       </div>
     );
   }
